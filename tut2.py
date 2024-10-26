6f7b5bd686b95fa956d5c24b6bde3703ec62ce4d
Vivek Kumar Mishra(308083) <vivek571@gmail.com>
2024-10-26 21:08:37 +0530

    Adding tut2 file

---
+++ tut2.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+a=np.empty((4,4), dtype=int)
+print(a)
+
+y1=np.zeros((4,4), dtype=int)
+print(y1)
+
+y1=np.ones((4,4), dtype=int)
+print(y1)
+
+
+b=np.empty(6)
+print(b)
+
+b=np.zeros(6)
+print(b)
+
+b=np.ones(6)
+print(b)
+
+y=np.zeros((4,4), dtype=str)
+print(y)
+
+b=np.ones((4,4), dtype=int)
+print(b)
+
+y1=np.ones((4,4), dtype=str)
+print(y1)
+
+y1=np.ones((4,4), dtype=bool)
+print(y1)
+
