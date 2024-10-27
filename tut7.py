afcc7d03835ba6d105dabafa99be6283d954d978
Vivek Kumar Mishra(308083) <vivek571@gmail.com>
2024-10-27 17:28:12 +0530

    random numbers

---
+++ tut7.py
@@ -0,0 +1,21 @@
+import numpy as np
+print("vivek")
+
+# print(np.random.random())
+# print(np.random.random(1))
+# print(np.random.random(2))
+# print(np.random.random((2,2)))
+
+# print(np.random.randint(1,10))
+
+# print(np.random.randint(1,10, (2,2)))
+
+#3d array
+# print(np.random.randint(1,10, (3,3,4)))
+
+# print(np.random.rand(2,2))
+
+# print(np.random.randn(2,2))
+
+a=np.arange(1,10)
+print(np.random.choice(a))
